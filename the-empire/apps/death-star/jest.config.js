1eb0f86d94231c488388acb0d37f89b02c186ebc
mrman888 <amiibospam@gmail.com>
2021-09-26 14:49:20 +0200

    initial

---
+++ the-empire/apps/death-star/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'death-star',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/death-star',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
